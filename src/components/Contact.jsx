9404cac09e7b7fbac55da18ae6fda32bb2a15ac4
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-09-08 13:09:02 +0400

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
             <form method="POST" action="https://getform.io/f/d28adb52-ccbc-480c-af8d-9edee6091415" className='flex flex-col max-w-[600px] w-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                    <p className='text-gray-300 py-4'>// Submit the from below or send me an email - sandrex233@gmail.com</p>
+                    <p className='text-gray-300 py-4'>{'//'} Submit the from below or send me an email - sandrex233@gmail.com</p>
                 </div>
                 <input className="bg-[#ccd6f6] p-2" type="text" name="name" placeholder='Name' />
                 <input className='my-4 p-2 bg-[#ccd6f6]' type="email" name="email" placeholder='Email' />
