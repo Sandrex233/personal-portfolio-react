a6cce40dcd811a8443751700be287a5bbdd7aca0
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-09-11 08:14:06 +0400

    desc change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
                 <p className='text-pink-600'>Hi, my name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Sandro Tsertsvadze</h1>
                 <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front-End Web Developer</h2>
-                <p className='text-[#8892b0] py-4 max-w-[700px]'>I’m a full-stack developer specializing in building (and occasionally
+                <p className='text-[#8892b0] py-4 max-w-[700px]'>I’m a front-end developer specializing in building (and occasionally
                     designing) exceptional digital experiences. Currently, I’m focused on
-                    building responsive full-stack web applications.</p>
+                    building responsive front-end web applications.</p>
                 <div>
                     <Link to="work" smooth={true} duration={500}>
                         <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work
