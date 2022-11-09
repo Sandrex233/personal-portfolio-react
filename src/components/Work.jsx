ed3df7e35bab3bd4ed651b16cf84766e3df05f4f
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-11-09 19:00:22 +0400

    removes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import Manage from '../assets/manage.jpg'
-import Fylo from '../assets/fylo.jpg'
+
 import EasyBank from '../assets/easybank.jpg'
-import Loopstudios from '../assets/loopstudios.jpg'
 import Urlshortening from '../assets/urlshortening.jpg'
 import Bookmark from '../assets/bookmark.jpg'
 
@@ -23,46 +21,6 @@ const Work = () => {
 
                     {/* Grid Item */}
                     <div
-                        style={{ backgroundImage: `url(${Fylo})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                React JS Application
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="https://dainty-biscotti-ed0c07.netlify.app/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href="https://github.com/Sandrex233/fylo-dark-theme-landing-page">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div
-                        style={{ backgroundImage: `url(${Manage})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                Manage landing page
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="https://voluble-chaja-b9260f.netlify.app/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href="https://github.com/Sandrex233/manage-landing-page-master">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div
                         style={{ backgroundImage: `url(${EasyBank})` }}
                         className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
@@ -82,25 +40,7 @@ const Work = () => {
                         </div>
                     </div>
 
-                    <div
-                        style={{ backgroundImage: `url(${Loopstudios})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                Loopstudios Page
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="https://bespoke-seahorse-b25be4.netlify.app/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href="https://github.com/Sandrex233/loopstudios-landing-page">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
 
                     <div
                         style={{ backgroundImage: `url(${Urlshortening})` }}
