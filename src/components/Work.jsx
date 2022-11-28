79df7b095efbd32f0c57745e2b866f516dd25b96
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-11-28 11:08:45 +0400

    rest+countries

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import EasyBank from '../assets/easybank.jpg'
 import Urlshortening from '../assets/urlshortening.jpg'
 import Bookmark from '../assets/bookmark.jpg'
+import restCountries from '../assets/rest-countries.jpg'
 
 
 
@@ -76,6 +77,26 @@ const Work = () => {
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
                                 <a href="https://github.com/Sandrex233/bookmark-landing-page">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        style={{ backgroundImage: `url(${restCountries})` }}
+                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                REST Countries API
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="https://rest-countries-api-reactjs-tailwind.netlify.app//">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href="https://github.com/Sandrex233/rest-countries-api">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
                             </div>
